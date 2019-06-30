784739a2f9c7020038e6048a35cadc164f6ce00b
Pjae215 <lala2462002@hotmail.com>
2019-06-30 12:44:33 -0400

    created on click functions

--- assets/schedule.js
+++ assets/schedule.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
         apiKey: "AIzaSyC3t4RIETrwmLIzTrCGl_BltItY4K6s4Ng",
         authDomain: "classstuffpjae.firebaseapp.com",
         databaseURL: "https://classstuffpjae.firebaseio.com/"
-    };
+                };
     firebase.initializeApp(config);
-
-    
+//need to revisit trainschedule database...this one is temporary
+   
     // database var
     var database = firebase.database();
 
@@ -16,5 +16,31 @@ $(document).ready(function(){
     var destination;
     var firstTrain;
     var frequency = 0;
+   
+    $("#add-train").on("click", function() {
+    event.preventDefault();
+
+    //error in getting validation to submit to database
+    //$("#form-group").checkValidity();
+    
+       
+        // Storing and retreiving new train data
+        name = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+
+
+        // Pushing to database
+        database.ref().push({
+            name: name,
+            destination: destination,
+            firstTrain: firstTrain,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        
+        $("form")[0].reset();
+    });
 
     
